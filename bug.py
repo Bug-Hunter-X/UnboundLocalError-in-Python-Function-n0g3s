8489812fa35be93fd53f82c05a1706db49897680
BugHunter-X <askzenai@gmail.com>
2025-01-31 12:26:05 +0100

    Add bug.py file

---
+++ bug.py
@@ -0,0 +1,14 @@
+def function_with_uncommon_error(a, b):
+    try:
+        result = a / b
+        return result
+    except ZeroDivisionError:
+        print("Error: Division by zero")
+        return None
+    except TypeError:
+        print("Error: Unsupported operand type(s) for / ")
+        return None
+
+# The following line will cause an UnboundLocalError
+# because 'result' is only defined inside the try block
+print(result)
